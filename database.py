e8ad239874ef342d91f9dba8b2f23c4a949b73df
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-03-21 10:38:57 +0000

    Update database.py

--- database.py
+++ database.py
@@ -6,15 +6,16 @@ from deta import Deta  # pip install deta
 DETA_KEY = st.secrets["DETA_KEY"]
 
 # Initialize with a project key
+DETA_KEY = "a0xtf7euf1h_iS7QUeEZh2TpeUnQ9B92NcRWxCgFU1uW"
 deta = Deta(DETA_KEY)
 
 # This is how to create/connect a database
-db = deta.Base("monthly_reports")
+db = deta.Base("employee_data1")
 
 
 def insert_period(period, incomes, expenses, comment):
     """Returns the report on a successful creation, otherwise raises an error"""
-    return db.put({"key": period, "incomes": incomes, "expenses": expenses, "comment": comment})
+    return db.put({"key": Employee Number, "incomes": incomes, "expenses": expenses, "comment": comment})
 
 
 def fetch_all_periods():
